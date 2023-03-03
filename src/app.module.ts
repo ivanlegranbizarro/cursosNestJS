536698eac985af3f01ca32725d52427aec524b32
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-03 12:18:29 +0100

    variables de entorno seteadas globalmente

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,18 @@ import { CoursesModule } from './courses/courses.module';
 import { AuthModule } from './auth/auth.module';
 import { VideosModule } from './videos/videos.module';
 import { AwardsModule } from './awards/awards.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CoursesModule, AuthModule, VideosModule, AwardsModule],
+  imports: [
+    CoursesModule,
+    AuthModule,
+    VideosModule,
+    AwardsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
