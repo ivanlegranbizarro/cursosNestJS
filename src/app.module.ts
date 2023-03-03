ff7c81bca001e4ac5b1eae4c10d70f2254625bd0
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-03 12:29:03 +0100

    covering Index

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { AuthModule } from './auth/auth.module';
 import { VideosModule } from './videos/videos.module';
 import { AwardsModule } from './awards/awards.module';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -15,6 +17,9 @@ import { ConfigModule } from '@nestjs/config';
     AwardsModule,
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
   ],
   controllers: [AppController],
